4ecd2a87e89dd5b7587d2f68ca8399d19f9fb48a
daideptrai <dain@joblogic.com>
2024-01-12 11:58:42 +0700

    implement card

--- src/server/graphql/validators/card.js
+++ src/server/graphql/validators/card.js
@@ -3,11 +3,11 @@ import JoiObjectId from 'joi-objectid';
 
 Joi.objectId = JoiObjectId(Joi);
 
-export const findTheme = Joi.object().keys({
+export const findCard = Joi.object().keys({
   id: Joi.objectId()
 });
 
-export const createTheme = Joi.object().keys({
+export const createCard = Joi.object().keys({
   userId: Joi.objectId(),
   themeId: Joi.objectId(),
   config: Joi.string()
