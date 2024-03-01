9670f4bafccf1edfd11e5b87742aa9971c8a0ad3
unknown <ngdai0402@gmail.com>
2024-03-01 19:46:45 +0700

    following esm

--- graphql/schemas/schemas.js
+++ graphql/schemas/schemas.js
@@ -1,8 +1,8 @@
-import user from './user';
-import root from './root';
-import theme from './theme';
-import card from './card';
-import tag from './tag';
-import blog from './blog';
+import user from './user.js';
+import root from './root.js';
+import theme from './theme.js';
+import card from './card.js';
+import tag from './tag.js';
+import blog from './blog.js';
 
 export default [user, root, theme, card, tag, blog];
