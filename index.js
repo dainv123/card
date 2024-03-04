2566f52fa98a1d8d7a7cf329c8cdf042deb73bcc
daideptrai <dain@joblogic.com>
2024-03-04 10:37:27 +0700

    change config

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import resolvers from './graphql/resolvers/resolvers.js';
 import schemaDirectives from './graphql/directives/directives.js';
 import fileUpload from 'express-fileupload';
 import uploadRoutes from './api/upload.js';
-import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServer
+} from 'apollo-server-express';
 
 const {
   PORT,
@@ -36,12 +38,15 @@ if (NODE_ENV === 'development') {
 
 app.use(helmet());
 app.use(helmet.permittedCrossDomainPolicies());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({
+  limit: '1mb'
+}));
 app.use(fileUpload());
 app.use('/api', uploadRoutes);
 app.use('/uploads', express.static(__dirname + UPLOADS_FOLDER));
 app.use('/themes', express.static(__dirname + '/client/themes'));
 app.use('/public', express.static(__dirname + '/client/public'));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('/*', (req, res) => res.sendFile(__dirname + '/client/dist/index.html'))
 
 // Set User Session
@@ -51,7 +56,9 @@ mongoose.set('useCreateIndex', true);
 
 app.use(
   session({
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    }),
     name: SESSION_NAME,
     secret: SESSION_SECRET,
     resave: true,
@@ -70,16 +77,20 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   schemaDirectives,
-  playground:
-    NODE_ENV.trim() !== 'development'
-      ? false
-      : {
-        settings: {
-          'request.credentials': 'include',
-          'schema.polling.enable': false
-        }
-      },
-  context: ({ req, res }) => ({ req, res })
+  playground: NODE_ENV.trim() !== 'development' ?
+    false : {
+      settings: {
+        'request.credentials': 'include',
+        'schema.polling.enable': false
+      }
+    },
+  context: ({
+    req,
+    res
+  }) => ({
+    req,
+    res
+  })
 });
 
 // Logging with Morgan
@@ -102,7 +113,9 @@ mongoose.connect(MONGO_DB_URI, {
 
 mongoose.connection.once('open', () => {
   const port = PORT || 8080;
-  app.listen({ port }, () => {
+  app.listen({
+    port
+  }, () => {
     console.log(`Server running on port ${port}`);
   });
 });
