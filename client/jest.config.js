6a559ac5cf04b0ecfd2fc22410431f21a350a135
DESKTOP-O5QRDLQ\Admin <ngdai0402@gmail.com>
2024-04-02 20:50:43 +0700

    setup jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.css$': 'C:/Local/Project/Github/card/client/empty-module.js' 
+    // '\\.css$': 'D:/Project/Github/react-express-mongo-boilerplate/client/empty-module.js',
+    '\\.css$': 'C:/Local/Project/Github/card/client/empty-module.js', 
+    '\\.less$': 'C:/Local/Project/Github/card/client/empty-module.js' 
   },
 };
