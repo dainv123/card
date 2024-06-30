0fa213cd036ef1f9bcd7875c0c2b74ee7639fe85
DESKTOP-O5QRDLQ\Admin <ngdai0402@gmail.com>
2024-06-30 18:35:56 +0700

    setup ci github

--- client/__mocks__/empty-module.js
+++ client/__mocks__/empty-module.js
@@ -1,1 +1,2 @@
+// empty-module.js
 module.exports = {};
--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.css$': 'D:/Project/Github/react-express-mongo-boilerplate/client/empty-module.js',
-    '\\.less$': 'D:/Project/Github/react-express-mongo-boilerplate/client/empty-module.js' 
+    '\\.css$': '<rootDir>/client/__mocks__/empty-module.js',
+    '\\.less$': '<rootDir>/client/__mocks__/empty-module.js' 
   },
 };
